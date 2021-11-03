c197a990eeb7f9466c1038446063b8ee773c53c4
Piyush Harsh <76912417+piyush-harsh-15@users.noreply.github.com>
2021-11-03 10:30:46 +0530

    upload

---
+++ return.py
@@ -0,0 +1,7 @@
+def cube(num):
+    return num*num*num
+    #after a return statement the control goes out from the block
+
+result = cube(5)
+
+print(result)
---
+++ reverse integer.py
@@ -0,0 +1,10 @@
+n = input("Enter a number: ")
+num = int(n)
+rev = 0
+ 
+while(num > 0):
+    a = num % 10
+    rev = rev * 10 + a
+    num = num // 10
+ 
+print(rev)
