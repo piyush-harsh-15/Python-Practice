f0d070e0edb7256284c2d224354aa8af42b1713e
Piyush Harsh <76912417+piyush-harsh-15@users.noreply.github.com>
2021-11-04 06:10:34 +0530

    upload

---
+++ ifelse.py
@@ -0,0 +1,21 @@
+#Conditional Statements: If-elif-Else
+
+# or - either this or that fits the condition 
+# and - both should fit the condition 
+# not - negation of the expression 
+
+def largest():
+    n1 = input("Enter the first num: ")
+    n2 = input("Enter the second num: ")
+
+    if n1 == n2 : 
+        print("The two numbers are equal")
+    else: 
+        print("The two numbers are not equal")
+
+largest()
+
+# if expression: task1
+# elif expression: task2
+# else: task3
+
